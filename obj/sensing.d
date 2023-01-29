710435e351df826b9d7ca1cd7c248516af1ed087
she743 <she743@korea.ac.kr>
2023-01-29 09:31:12 +0000

    Update git ignore file and ignore the object files for pushing on Github

--- obj/sensing.d
+++
@@ -1,2 +0,0 @@
-obj/sensing.o: src/sensing.cpp /usr/include/stdc-predef.h src/sensing.h \
- /usr/local/include/wiringPi.h
--- obj/tcrt.d
+++
@@ -1,2 +0,0 @@
-obj/tcrt.o: src/tcrt.cpp /usr/include/stdc-predef.h src/tcrt.h \
- /usr/local/include/wiringPi.h
